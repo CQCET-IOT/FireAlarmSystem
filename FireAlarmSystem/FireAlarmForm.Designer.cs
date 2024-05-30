c9a973889035a51b8ddced34108de2dd0bf61192
morgen <morgengc@outlook.com>
2024-05-30 09:43:58 +0800

    添加功能代码

---
+++ FireAlarmSystem/FireAlarmForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace FireAlarmSystem
+{
+    partial class FireAlarmForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb_DeviceKey = new System.Windows.Forms.Label();
+            this.lb_DeviceName = new System.Windows.Forms.Label();
+            this.lb_ProductId = new System.Windows.Forms.Label();
+            this.tb_DeviceKey = new System.Windows.Forms.TextBox();
+            this.tb_DeviceName = new System.Windows.Forms.TextBox();
+            this.tb_ProductId = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lb_AiSecret = new System.Windows.Forms.Label();
+            this.lb_AiKey = new System.Windows.Forms.Label();
+            this.tb_AiSecret = new System.Windows.Forms.TextBox();
+            this.tb_AiKey = new System.Windows.Forms.TextBox();
+            this.tb_Smoke = new System.Windows.Forms.TextBox();
+            this.lb_Smoke = new System.Windows.Forms.Label();
+            this.bt_FireDetect = new System.Windows.Forms.Button();
+            this.bt_UploadProperty = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_DeviceKey);
+            this.groupBox1.Controls.Add(this.lb_DeviceName);
+            this.groupBox1.Controls.Add(this.lb_ProductId);
+            this.groupBox1.Controls.Add(this.tb_DeviceKey);
+            this.groupBox1.Controls.Add(this.tb_DeviceName);
+            this.groupBox1.Controls.Add(this.tb_ProductId);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(373, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OneNET-Studio参数";
+            // 
+            // lb_DeviceKey
+            // 
+            this.lb_DeviceKey.AutoSize = true;
+            this.lb_DeviceKey.Location = new System.Drawing.Point(25, 87);
+            this.lb_DeviceKey.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_DeviceKey.Name = "lb_DeviceKey";
+            this.lb_DeviceKey.Size = new System.Drawing.Size(47, 12);
+            this.lb_DeviceKey.TabIndex = 2;
+            this.lb_DeviceKey.Text = "设备KEY";
+            // 
+            // lb_DeviceName
+            // 
+            this.lb_DeviceName.AutoSize = true;
+            this.lb_DeviceName.Location = new System.Drawing.Point(20, 57);
+            this.lb_DeviceName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_DeviceName.Name = "lb_DeviceName";
+            this.lb_DeviceName.Size = new System.Drawing.Size(53, 12);
+            this.lb_DeviceName.TabIndex = 2;
+            this.lb_DeviceName.Text = "设备名称";
+            // 
+            // lb_ProductId
+            // 
+            this.lb_ProductId.AutoSize = true;
+            this.lb_ProductId.Location = new System.Drawing.Point(31, 26);
+            this.lb_ProductId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_ProductId.Name = "lb_ProductId";
+            this.lb_ProductId.Size = new System.Drawing.Size(41, 12);
+            this.lb_ProductId.TabIndex = 1;
+            this.lb_ProductId.Text = "产品ID";
+            // 
+            // tb_DeviceKey
+            // 
+            this.tb_DeviceKey.Location = new System.Drawing.Point(80, 84);
+            this.tb_DeviceKey.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_DeviceKey.Multiline = true;
+            this.tb_DeviceKey.Name = "tb_DeviceKey";
+            this.tb_DeviceKey.Size = new System.Drawing.Size(279, 19);
+            this.tb_DeviceKey.TabIndex = 0;
+            this.tb_DeviceKey.Text = "KRnjkUdlFWVhpYM5YXH1nwFTdT+1t23DoxWtwSFDix8=";
+            // 
+            // tb_DeviceName
+            // 
+            this.tb_DeviceName.Location = new System.Drawing.Point(80, 54);
+            this.tb_DeviceName.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_DeviceName.Multiline = true;
+            this.tb_DeviceName.Name = "tb_DeviceName";
+            this.tb_DeviceName.Size = new System.Drawing.Size(279, 19);
+            this.tb_DeviceName.TabIndex = 0;
+            this.tb_DeviceName.Text = "HTTP01";
+            // 
+            // tb_ProductId
+            // 
+            this.tb_ProductId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_ProductId.Location = new System.Drawing.Point(80, 23);
+            this.tb_ProductId.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_ProductId.Multiline = true;
+            this.tb_ProductId.Name = "tb_ProductId";
+            this.tb_ProductId.Size = new System.Drawing.Size(278, 19);
+            this.tb_ProductId.TabIndex = 0;
+            this.tb_ProductId.Text = "764c73v9oD";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tb_Smoke);
+            this.splitContainer1.Panel2.Controls.Add(this.lb_Smoke);
+            this.splitContainer1.Panel2.Controls.Add(this.bt_FireDetect);
+            this.splitContainer1.Panel2.Controls.Add(this.bt_UploadProperty);
+            this.splitContainer1.Size = new System.Drawing.Size(389, 397);
+            this.splitContainer1.SplitterDistance = 250;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lb_AiSecret);
+            this.groupBox2.Controls.Add(this.lb_AiKey);
+            this.groupBox2.Controls.Add(this.tb_AiSecret);
+            this.groupBox2.Controls.Add(this.tb_AiKey);
+            this.groupBox2.Location = new System.Drawing.Point(7, 141);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(373, 89);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "AI平台参数";
+            // 
+            // lb_AiSecret
+            // 
+            this.lb_AiSecret.AutoSize = true;
+            this.lb_AiSecret.Location = new System.Drawing.Point(7, 57);
+            this.lb_AiSecret.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_AiSecret.Name = "lb_AiSecret";
+            this.lb_AiSecret.Size = new System.Drawing.Size(65, 12);
+            this.lb_AiSecret.TabIndex = 2;
+            this.lb_AiSecret.Text = "Secret Key";
+            // 
+            // lb_AiKey
+            // 
+            this.lb_AiKey.AutoSize = true;
+            this.lb_AiKey.Location = new System.Drawing.Point(31, 26);
+            this.lb_AiKey.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_AiKey.Name = "lb_AiKey";
+            this.lb_AiKey.Size = new System.Drawing.Size(41, 12);
+            this.lb_AiKey.TabIndex = 1;
+            this.lb_AiKey.Text = "AI Key";
+            // 
+            // tb_AiSecret
+            // 
+            this.tb_AiSecret.Location = new System.Drawing.Point(80, 54);
+            this.tb_AiSecret.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_AiSecret.Multiline = true;
+            this.tb_AiSecret.Name = "tb_AiSecret";
+            this.tb_AiSecret.Size = new System.Drawing.Size(279, 19);
+            this.tb_AiSecret.TabIndex = 0;
+            this.tb_AiSecret.Text = "9865503bdca74b41bcaa8b1b06354d78";
+            // 
+            // tb_AiKey
+            // 
+            this.tb_AiKey.Location = new System.Drawing.Point(80, 23);
+            this.tb_AiKey.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_AiKey.Multiline = true;
+            this.tb_AiKey.Name = "tb_AiKey";
+            this.tb_AiKey.Size = new System.Drawing.Size(279, 19);
+            this.tb_AiKey.TabIndex = 0;
+            this.tb_AiKey.Text = "66b2848d2eab4165a45a6120233b4d18";
+            // 
+            // tb_Smoke
+            // 
+            this.tb_Smoke.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Smoke.Location = new System.Drawing.Point(76, 29);
+            this.tb_Smoke.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Smoke.Multiline = true;
+            this.tb_Smoke.Name = "tb_Smoke";
+            this.tb_Smoke.Size = new System.Drawing.Size(214, 19);
+            this.tb_Smoke.TabIndex = 2;
+            // 
+            // lb_Smoke
+            // 
+            this.lb_Smoke.AutoSize = true;
+            this.lb_Smoke.Location = new System.Drawing.Point(17, 32);
+            this.lb_Smoke.Name = "lb_Smoke";
+            this.lb_Smoke.Size = new System.Drawing.Size(53, 12);
+            this.lb_Smoke.TabIndex = 1;
+            this.lb_Smoke.Text = "烟雾浓度";
+            // 
+            // bt_FireDetect
+            // 
+            this.bt_FireDetect.Location = new System.Drawing.Point(124, 77);
+            this.bt_FireDetect.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_FireDetect.Name = "bt_FireDetect";
+            this.bt_FireDetect.Size = new System.Drawing.Size(118, 45);
+            this.bt_FireDetect.TabIndex = 0;
+            this.bt_FireDetect.Text = "上传文件至AI平台进行火情检测";
+            this.bt_FireDetect.UseVisualStyleBackColor = true;
+            this.bt_FireDetect.Click += new System.EventHandler(this.bt_FireDetect_Click);
+            // 
+            // bt_UploadProperty
+            // 
+            this.bt_UploadProperty.Location = new System.Drawing.Point(294, 26);
+            this.bt_UploadProperty.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_UploadProperty.Name = "bt_UploadProperty";
+            this.bt_UploadProperty.Size = new System.Drawing.Size(74, 25);
+            this.bt_UploadProperty.TabIndex = 0;
+            this.bt_UploadProperty.Text = "上传属性";
+            this.bt_UploadProperty.UseVisualStyleBackColor = true;
+            this.bt_UploadProperty.Click += new System.EventHandler(this.bt_UploadProperty_Click);
+            // 
+            // FireAlarmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 397);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FireAlarmForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "森林火灾险情检测";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lb_DeviceKey;
+        private System.Windows.Forms.Label lb_DeviceName;
+        private System.Windows.Forms.Label lb_ProductId;
+        private System.Windows.Forms.TextBox tb_DeviceKey;
+        private System.Windows.Forms.TextBox tb_DeviceName;
+        private System.Windows.Forms.TextBox tb_ProductId;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lb_AiSecret;
+        private System.Windows.Forms.Label lb_AiKey;
+        private System.Windows.Forms.TextBox tb_AiSecret;
+        private System.Windows.Forms.TextBox tb_AiKey;
+        private System.Windows.Forms.Button bt_UploadProperty;
+        private System.Windows.Forms.Button bt_FireDetect;
+        private System.Windows.Forms.TextBox tb_Smoke;
+        private System.Windows.Forms.Label lb_Smoke;
+    }
+}
+
---
+++ FireAlarmSystem/FireAlarmForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json; 
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace FireAlarmSystem
+{
+    public partial class FireAlarmForm : Form
+    {
+        public FireAlarmForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 烟雾浓度属性上传
+        /// 官方文档：https://open.iot.10086.cn/doc/iot_platform/book/device-connect&manager/HTTP/api/devicePropertyNotify.html
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_UploadProperty_Click(object sender, EventArgs e)
+        {
+            string productId = tb_ProductId.Text.Trim();
+            string deviceName = tb_DeviceName.Text.Trim();
+            string deviceKey = tb_DeviceKey.Text.Trim();
+            double smoke = double.Parse(tb_Smoke.Text.Trim());
+
+            JObject payload = GenSmokeProperty(smoke);
+            UploadProperty(productId, deviceName, deviceKey, payload);
+        }
+
+        /// <summary>
+        /// 火情检测
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_FireDetect_Click(object sender, EventArgs e)
+        {
+            // 创建文件选择对话框
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择上传的图片";
+            ofd.Filter = "图片格式|*.jpg";
+            ofd.Multiselect = false;
+
+            string fileFullName = null;
+            if (ofd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            // 获得文件的完整名称，包括路径和文件名
+            fileFullName = ofd.FileName;
+
+            // 第一步：上传文件到AI平台，并获得火情检验的结果
+            string aiKey = tb_AiKey.Text.ToString();
+            string secretKey = tb_AiSecret.Text.ToString();
+            string label = null;
+            double confidence = 0;
+            if (!FireDetect(fileFullName, aiKey, secretKey, ref label, ref confidence))
+                return;
+
+            // 第二步：将该文件上传到OneNET Studio平台进行保存，以便追溯
+            string productId = tb_ProductId.Text.Trim();
+            string deviceName = tb_DeviceName.Text.Trim();
+            string deviceKey = tb_DeviceKey.Text.Trim();
+            string uuid = UploadFile(fileFullName, productId, deviceName, deviceKey);
+
+            // 第三步：将识别结果和文件uuid作为属性上传OneNET Studio
+            JObject payload = GenAiRecordProperty(uuid, label, confidence);
+            UploadProperty(productId, deviceName, deviceKey, payload);
+        }
+
+ 
+        /// <summary>
+        /// 上传图片到AI平台，并完成火情检测
+        /// 官方文档：https://open.iot.10086.cn/ai/helpCenter/technicalDoc?id=c6
+        /// </summary>
+        /// <param name="fileFullName">文件全名</param>
+        /// <param name="aiKey">AI Key</param>
+        /// <param name="secretKey">Secret Key</param>
+        /// <param name="label">AI识别的标签</param>
+        /// <param name="confidence">置信度</param>
+        /// <returns></returns>
+        private bool FireDetect(string fileFullName, string aiKey, string secretKey, ref string label, ref double confidence)
+        {
+            string url = string.Format("http://ai.heclouds.com:9090/v1/aiApi/picture/FIRE_DETECTION");
+
+            string token = AiPlatform.GetAccessToken(aiKey, secretKey);
+            //MessageBox.Show(token);
+
+            JObject payload = GenFireDetectionPayload(fileFullName);
+
+            try
+            {
+                var client = new RestClient(url);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("token", token);
+                request.AddJsonBody(payload.ToString());
+
+                // 返回值处理
+                IRestResponse response = client.Execute(request);
+                if (AiResponseUtils.GetLabelAndConfidence(response, ref label, ref confidence) == OneNetResponseUtils.RESPONSE_ERROR)
+                {
+                    MessageBox.Show("AI识别失败!");
+                }
+                else
+                {
+                    MessageBox.Show("图片内容识别为 " + label + "，其置信度为 " + confidence);
+                    return true;
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 向OneNET Studio平台上传文件，该文件会关联到设备上
+        /// 官方文档： https://open.iot.10086.cn/doc/iot_platform/book/device-connect&manager/device-file-manager.html
+        /// </summary>
+        /// <param name="fileFullName">文件全名</param>
+        /// <param name="productId">产品ID</param>
+        /// <param name="deviceName">设备名称</param>
+        /// <param name="deviceKey">设备Key</param>
+        private string UploadFile(string fileFullName, string productId, string deviceName, string deviceKey)
+        {
+            // 获取文件名和文件大小
+            FileInfo fileInfo = new FileInfo(fileFullName);
+            string fileBaseName = fileInfo.Name;
+            long fileLength = fileInfo.Length;
+
+            string token = OneNetToken.AssembleSouthToken(productId, deviceName, deviceKey, DateTime.Now.AddDays(90));
+            //MessageBox.Show(token);
+
+            try
+            {
+                string url = string.Format("http://studio-file.heclouds.com/studio/{0}/{1}/outupload", productId, deviceName);
+                var client = new RestClient(url);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "multipart/form-data");
+                request.AddHeader("Authorization", token);
+
+                request.AddFile("file", fileFullName);
+                request.AddParameter("md5", FileUtils.CalFileMD5(fileFullName));
+                request.AddParameter("filename", fileBaseName);
+                request.AddParameter("size", fileLength);
+
+                // 返回值处理
+                IRestResponse response = client.Execute(request);
+                string uploadedFileId = OneNetResponseUtils.GetFileUuid(response);
+                //MessageBox.Show(uploadedFileId);
+                return uploadedFileId;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 上传属性至OneNET Studio平台
+        /// 官方文档：https://open.iot.10086.cn/doc/v5/develop/detail/690
+        /// </summary>
+        /// <param name="productId">产品ID</param>
+        /// <param name="deviceName">设备名称</param>
+        /// <param name="deviceKey">设备Key</param>
+        /// <param name="payload">上报数据</param>
+        /// <returns></returns>
+        private bool UploadProperty(string productId, string deviceName, string deviceKey, JObject payload)
+        {
+            string topic = string.Format("$sys/{0}/{1}/thing/property/post", productId, deviceName);
+            string url = string.Format("https://open.iot.10086.cn/studio/http/device/thing/property/post?protocol=http&topic={0}", topic);
+
+            string token = OneNetToken.AssembleSouthToken(productId, deviceName, deviceKey, DateTime.Now.AddDays(90));
+            //MessageBox.Show(token);
+
+            try
+            {
+                var client = new RestClient(url);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("token", token);
+                request.AddJsonBody(payload.ToString());
+
+                // 采用同步方式。如果是异步方式，会立即返回，造成json为空
+                IRestResponse response = client.Execute(request);
+                if (OneNetResponseUtils.GetErrNo(response) == OneNetResponseUtils.RESPONSE_OK)
+                {
+                    MessageBox.Show("属性上传成功，请前往OneNET Studio查看数据。");
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("属性上传失败！");
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 构造火灾检测的上传数据
+        /// </summary>
+        /// <param name="fileFullName">文件全名</param>
+        /// <returns></returns>
+        private JObject GenFireDetectionPayload(string fileFullName)
+        { 
+            JArray array = new JArray();
+            array.Add(FileUtils.FileToBase64Str(fileFullName));
+
+            JObject payload = new JObject();
+            payload["type"] = "GPU";
+            payload.Add("picture", array);
+            //MessageBox.Show(payload.ToString());
+
+            return payload;
+        }
+
+        /// <summary>
+        /// 构造烟雾浓度属性，参见物模型
+        /// </summary>
+        /// <param name="SmokeScope"></param>
+        /// <returns></returns>
+        private JObject GenSmokeProperty(double SmokeScope)
+        { 
+            JObject smokeScope = new JObject();
+            smokeScope["value"] = Math.Round(SmokeScope,2);
+
+            JObject paramList = new JObject();
+            paramList["SmokeScope"] = smokeScope;
+
+            Random rd = new Random();
+            JObject payload = new JObject();
+            payload["id"] = rd.Next(0, 100).ToString();
+            payload["version"] = "1.0";
+            payload["params"] = paramList;
+            //MessageBox.Show(payload.ToString());
+
+            return payload;
+        }
+
+        /// <summary>
+        /// 构造AI检测结果属性，参见物模型
+        /// </summary>
+        /// <param name="imgid">图片ID</param>
+        /// <param name="label">类型标签</param>
+        /// <param name="confidence">置信度</param>
+        /// <returns></returns>
+        private JObject GenAiRecordProperty(string imgid, string label, double confidence)
+        { 
+            JObject values = new JObject();
+            values["imgid"] = imgid;
+            values["label"] = label;
+            values["confidence"] = Math.Round(confidence, 2);
+
+            JObject aiRecord = new JObject();
+            aiRecord["value"] = values;
+
+            JObject paramList = new JObject();
+            paramList["AiRecord"] = aiRecord;
+
+            Random rd = new Random();
+            JObject payload = new JObject();
+            payload["id"] = rd.Next(0, 100).ToString();
+            payload["version"] = "1.0";
+            payload["params"] = paramList;
+            //MessageBox.Show(payload.ToString());
+
+            return payload;
+        }
+    }
+}
